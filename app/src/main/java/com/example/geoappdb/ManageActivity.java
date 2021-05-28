aefb5478492cb0f75b8a31283e57b1bc57338995
Kajman <kaim1396@gmail.com>
2021-05-28 18:54:06 +0200

    Something was wrong

--- app/src/main/java/com/example/geoappdb/ManageActivity.java
+++ app/src/main/java/com/example/geoappdb/ManageActivity.java
@@ -1,21 +1,28 @@
 package com.example.geoappdb;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManageActivity extends AppCompatActivity {
     QuestionDBHELPER questionDBHELPER;
     SQLiteDatabase sqldb=null;
     TextView tvdbinfo;
-    Button querybtn,entrybtn,coutnbtn;
+    Button querybtn,entrybtn,coutnbtn,displaybtn;
     EditText etcountry,etcapital,etpopulation,etcurrency;
+    ListView lvforquestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +42,10 @@ public class ManageActivity extends AppCompatActivity {
     public void insertEntry(View view) {
         String country = etcountry.getText().toString();
         String capital = etcapital.getText().toString();
-        int population = Integer.parseInt(etpopulation.getText().toString());
+        int population;
+        try {
+            population = Integer.parseInt(etpopulation.getText().toString());
+        } catch(NumberFormatException nfe) { population = 0; }
         String currency = etcurrency.getText().toString();
         if(!country.equals("")&&!capital.equals("")){
             ContentValues newRecord=new ContentValues();
@@ -46,8 +56,85 @@ public class ManageActivity extends AppCompatActivity {
             sqldb.insert(QuestionDBHELPER.TABLE_NAME,null,newRecord);
         }
     }
-
+    //QUERY OF ALL VALUE
     public void queryDatabase(View view) {
+        Cursor entries = sqldb.query(QuestionDBHELPER.TABLE_NAME,
+                                    QuestionDBHELPER.columns,
+                null,
+                null,
+                null,
+                null,null
+                );
+       // processCursor(entries);
+        createQuestionFromCursor(entries);
+    }
+    public void querySpecial(View v) {
+        // all National
+        String selection = QuestionDBHELPER.COUNTRY+"=?";
+        String[] selectionArguments = {"asd"};
+        Cursor entries = sqldb.query(QuestionDBHELPER.TABLE_NAME,
+                QuestionDBHELPER.columns,
+                selection,
+                selectionArguments,
+                null,
+                null,null);
+        processCursor(entries);
+    }
+    private void  createQuestionFromCursor(Cursor c){
+        ArrayList<Question> questionsFromQuery= new ArrayList<>();
+        c.moveToFirst();
+        String cols[] = c.getColumnNames();
+        while (!c.isAfterLast()){
+            String capital=null,country=null,currency=null;
+            Integer population=0;
+            for(int colnb=0;colnb<c.getColumnCount();colnb++){
+                switch (c.getType(colnb)){
+                    case Cursor.FIELD_TYPE_STRING:
+                        String value = c.getString(colnb);
+                        if(cols[colnb].equals(QuestionDBHELPER.COUNTRY)) country = value;
+                        else if(cols[colnb].equals(QuestionDBHELPER.CAPITAL)) capital = value;
+                        else if(cols[colnb].equals(QuestionDBHELPER.CURRENCY)) currency = value;
+                        break;
+                    case Cursor.FIELD_TYPE_INTEGER:
+                        population = c.getInt(colnb);
+                        break;
+                }
+            }
+            Question theNewQuestion = new Question(country,capital,population,currency);
+            questionsFromQuery.add(theNewQuestion);
+            c.moveToNext();
+        }
+        c.close();
+        QuestionAdapter ca = new QuestionAdapter(this,questionsFromQuery);
+        lvforquestion.setAdapter(ca);
+        //display layout
+        //NEW ACTIVITY
+    }
+
+
+    private void processCursor(Cursor c) {
+        ArrayList<String> allRecords = new ArrayList<>();
+        String entry = new String();    //for displaing content intoTV
+        c.moveToFirst();
+        String cols[] = c.getColumnNames();
+        while (!c.isAfterLast()){
+            entry="";
+            for(int colnb=0;colnb<c.getColumnCount();colnb++){
+                switch (c.getType(colnb)){
+                    case Cursor.FIELD_TYPE_STRING:
+                        entry+=cols[colnb]+": "+c.getString(colnb)+ " ";
+                        break;
+                    case Cursor.FIELD_TYPE_INTEGER:
+                        entry+="\n"+cols[colnb]+": "+c.getInt(colnb);
+                }
+            }
+         allRecords.add(entry);
+            c.moveToNext();
+        }
+        String allEntries = new String();
+        for (String line: allRecords)
+            allEntries+=line+"\n";
+        tvdbinfo.setText(allEntries);
     }
 
     public void countRecords(View view) {
@@ -65,22 +152,37 @@ public class ManageActivity extends AppCompatActivity {
 
     public void deleteDB(View view) {
         questionDBHELPER.deleteDB();
+        tvdbinfo.setText("");
     }
     private void enableButtons(boolean enable){
         entrybtn.setEnabled(enable);
         querybtn.setEnabled(enable);
         coutnbtn.setEnabled(enable);
+        displaybtn.setEnabled(enable);
     }
     private void getViewsReferences() {
         tvdbinfo = findViewById(R.id.tvdbinfo);
         entrybtn=findViewById(R.id.entrybtn);
         coutnbtn=findViewById(R.id.countbtn);
         querybtn=findViewById(R.id.querybtn);
+        displaybtn.findViewById(R.id.displaybtn);
         etcurrency = findViewById(R.id.etcurrency);
         etcapital= findViewById(R.id.etcapital);
         etpopulation = findViewById(R.id.etpopulation);
         etcountry = findViewById(R.id.etcountry);
+        lvforquestion = findViewById(R.id.lvforquestion);
     }
+
+
+
+
+
+
+
+
+
+
+
     // ====================EXAMPLE FROM LESSON=============//
     public void example(View view) {
         ContentValues cv = new ContentValues();
